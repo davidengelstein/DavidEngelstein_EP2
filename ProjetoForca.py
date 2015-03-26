1dd142f5deece187ac17f838960d25ed759c81c9
davidengelstein <davidengelstein@hotmail.com>
2015-03-26 15:53:54 -0300

    Commit2

--- ProjetoForca.py
+++ ProjetoForca.py
@@ -1,1 +1,38 @@
 #DavidEngelstein
+from turtle import *
+window = Screen()    
+window.bgcolor("lightblue")
+window.title("Jogo da Forca")
+
+
+speed(5)  
+penup()      
+setpos(-250,-200)
+
+
+pendown()
+color("black")
+
+
+def forca():  
+    
+    left(90)  
+    forward(400) 
+    right(90)  
+    forward(150)
+    right(90)  
+    forward(50) 
+    left(90)
+    forward(50)
+    left(90)
+    forward(100)
+    left(90)
+    forward(250)
+    left(90)
+    fd(450)
+    left(90)
+    forward(50)
+    penup()
+    fd(50)
+forca()
+  
